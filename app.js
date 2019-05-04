571bb245d29c8c1b38d83a87d0425bf18651278c
YA-CHEN <xiao6108@gmail.com>
2019-05-04 23:28:55 +0800

    Add index action and index view template

--- app.js
+++ app.js
@@ -23,25 +23,46 @@ db.once('open', ()=>{
 
 // 1) 在首頁一次瀏覽所有支出的清單
 app.get('/', (req, res) => {
-  return res.render('index')
+  Record.find((err, record) => {
+    if (err) return console.error(err)
+    return res.render('index', { 
+      record: record,
+      style: 'main.css',
+    })  
+  })
 })
 // 2) 在首頁看到所有支出清單的總金額
 app.get('/expanse', (req, res) => {
-  res.send('列出所有 expanse')
+  Record.find((err, record) => {
+    if (err) return console.error(err)
+    return res.render('index', { 
+      record: record,
+      style: 'main.css',
+    })  
+  })
 })
 // 3) 新增一筆支出
 app.get('/expanse/new', (req, res) => {
-  res.send('新增 expanse 頁面')
+  return res.render('new', {
+  style: 'new.css'
+  }
+  )
 })
 app.post('/expanse', (req, res) => {
-  res.send('建立 expanse')
+  return res.render('index',
+    {style: 'new.css'}
+  )
 })
 // 4) 編輯支出的所有屬性 (一次只能編輯一筆) 
 app.get('/expanse/:id/edit', (req, res) => {
-  res.send('修改 expanse 頁面')
+  return res.render('edit',
+    {style: 'edit.css'}
+  )
 })
 app.post('/expanse/:id', (req, res) => {
-  res.send('修改 expanse')
+  return res.render('edit', 
+    {style: 'edit.css'}
+  )
 })
 // 5) 刪除任何一筆支出 (一次只能刪除一筆)
 app.post('/expanse/:id/delete', (req, res) => {
