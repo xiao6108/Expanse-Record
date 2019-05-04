0f72cc3b661f661ba03c2247d92db4ac6a50bf6a
YA-CHEN <xiao6108@gmail.com>
2019-05-04 15:12:54 +0800

    Add route

--- app.js
+++ app.js
@@ -15,9 +15,35 @@ db.once('open', ()=>{
   console.log('mongodb connected')
 })
 
+// 1) 在首頁一次瀏覽所有支出的清單
 app.get('/', (req,res)=>{
   res.send('Hello')
 })
+// 2) 在首頁看到所有支出清單的總金額
+app.get('/expanse', (req, res) => {
+  res.send('列出所有 expanse')
+})
+// 3) 新增一筆支出
+app.get('/expanse/new', (req, res) => {
+  res.send('新增 expanse 頁面')
+})
+app.post('/expanse', (req, res) => {
+  res.send('建立 expanse')
+})
+// 4) 編輯支出的所有屬性 (一次只能編輯一筆) 
+app.get('/expanse/:id/edit', (req, res) => {
+  res.send('修改 expanse 頁面')
+})
+app.post('/expanse/:id', (req, res) => {
+  res.send('修改 expanse')
+})
+// 5) 刪除任何一筆支出 (一次只能刪除一筆)
+app.post('/expanse/:id/delete', (req, res) => {
+  res.send('刪除 expanse')
+})
+
+
+
 
 app.listen(3000,()=>{
   console.log('app is running')
