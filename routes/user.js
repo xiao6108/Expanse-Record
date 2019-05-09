26dfcc8a75031cdbb551a4bb5b37cf829027b130
YA-CHEN <xiao6108@gmail.com>
2019-05-09 15:45:18 +0800

    Install bcryptjs

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs') 
 
 // 登入頁面
 router.get('/login', (req, res) => {
